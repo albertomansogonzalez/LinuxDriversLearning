d60c440a8f7ac7400ba168ffd941c1e018602c26
Alberto Manso <albertomanso@outlook.com>
2025-08-17 18:55:13 +0200

    Add commment

--- 07_file_operations/main.c
+++ 07_file_operations/main.c
@@ -1,3 +1,12 @@
+/*
+ Compilar y ejecutar este codigo para mostrar ejemplo
+ de comunicacion con el driver.
+
+ Este programa sera un proceso ejecutado en espacio de usuario
+ que se comunica con el driver (en espacio del kernel) mediante
+ llamadas al sistema.
+
+*/
 #include <stdio.h>
 
 int main(){
@@ -5,7 +14,7 @@ int main(){
 	printf("Presiona enter para abrir /dev/mydevice");
 	getchar();
 
-	FILE *f = fopen("/dev/mydevice", "r");
+	FILE *f = fopen("/dev/mydevice", "r"); // ejecuta llamada al sistema open()
 	if (f == NULL){
 		perror("No se puede abrir el fichero\n");
 		return 1;
@@ -15,7 +24,7 @@ int main(){
 
 	printf("Presiona una tecla para cerrar /dev/mydevice");
 	getchar();
-	fclose(f);
+	fclose(f); // ejecuta llamada al sistema close() (.release)
 
 	return 0;
 }
